05c158140168986cb9d3cccc3f247d816cc4ff76
Trần Phúc Thành <enigma15012001@gmail.com>
2021-10-31 00:40:01 +0700

    done learning dev ed

--- redux-dev-ed/src/index.js
+++ redux-dev-ed/src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore, compose, combineReducers } from "redux";
+import { counterReducer, isLoggedReducer } from "./reducers";
+import thunkMiddleware from "redux-thunk";
+import { Provider } from "react-redux";
+
+const allReducers = combineReducers({
+  counter: counterReducer,
+  isLogged: isLoggedReducer,
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  allReducers,
+  composeEnhancer(applyMiddleware(thunkMiddleware))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
